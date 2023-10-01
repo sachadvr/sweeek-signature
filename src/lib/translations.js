e30238d61c1df2c2f983c1c621b5cf2d45b9ca35
sachadvr <sachadvr@icloud.com>
2023-10-01 19:26:50 +0200

    Adding socialMedias links and adding NL/DE

--- src/lib/translations.js
+++ src/lib/translations.js
@@ -7,13 +7,15 @@ const translations = {
       email: 'Email',
       phone: 'Phone',
       uploadImage: 'Upload a Custom Image',
+      gmail: 'Gmail Access',
       copyButton: 'Click to Copy',
       currentJob: 'Symfony Fullstack Developer',
       saveMessage: 'Your signature has just been copied to your clipboard!',
       examplePreviewEmail: 'See here how it will look :',
+      currentEmail: 'sduvivier@sweeek.co.uk',
       signatureInstructions: `To add or modify your signature in Gmail, follow these simple steps:
 
-1. Click on the gear icon in the top right corner of your Gmail inbox.
+1. Click on the gear icon in the top right corner of your Gmail inbox. {gmail}
 2. Select "See all settings" and go to the "General" tab.
 3. Scroll down to the "Signature" section where you can create or edit your personalized signature. Paste the text you have in the clipboard here.
 4. Save the changes at the bottom of the page to apply your new signature.`,
@@ -32,6 +34,8 @@ const translations = {
   See you soon!
 
   Best regards,`,
+      linkFacebook: 'https://www.facebook.com/sweeek.uk',
+      linkSweeek: 'https://www.sweeek.co.uk',
     },
     fr: {
       signature: 'Votre signature personnalisée {brand}',
@@ -43,11 +47,13 @@ const translations = {
       uploadImage: 'Télécharger une image personnalisée',
       copyButton: 'Clique pour Copier',
       currentJob: 'Développeur Fullstack',
+      gmail: 'Accéder à Gmail',
       saveMessage: 'Votre signature vient d\'être copiée dans votre presse-papiers !',
       examplePreviewEmail: 'Voici un exemple d\'email :',
+      currentEmail: 'sduvivier@sweeek.fr',
       signatureInstructions: `Pour ajouter ou modifier votre signature sur Gmail, suivez ces étapes simples :
 
-      1. Cliquez sur l'icône d'engrenage en haut à droite de votre boîte de réception Gmail.
+      1. Cliquez sur l'icône d'engrenage en haut à droite de votre boîte de réception Gmail. {gmail}
       2. Sélectionnez "Voir tous les paramètres" puis allez dans l'onglet "Général".
       3. Faites défiler vers le bas jusqu'à la section "Signature" où vous pouvez créer ou éditer votre signature personnalisée. Collez le texte que vous avez dans le presse-papiers à cet endroit.
       4. Enregistrez les modifications en bas de la page pour appliquer votre nouvelle signature.`,
@@ -67,6 +73,10 @@ const translations = {
   À bientôt !
 
   Cordialement,`,
+      linkTwitter: 'https://twitter.com/sweeekfr',
+      linkPinterest: 'https://pinterest.fr/sweeekfr/',
+      linkFacebook: 'https://www.facebook.com/sweeek.fr',
+      linkSweeek: 'https://www.sweeek.fr',
     },
     es: {
       signature: 'Su {brand} firma personalizada',
@@ -75,14 +85,16 @@ const translations = {
       jobTitle: 'Puesto',
       email: 'Correo electrónico',
       phone: 'Teléfono',
+      gmail: 'Acceso a Gmail',
       currentJob: 'Desarrollador Symfony Fullstack',
       uploadImage: 'Subir una imagen personalizada',
+      currentEmail: 'sduvivier@sweeek.es',
       copyButton: 'Haga clic para copiar',
       examplePreviewEmail: 'Vea aquí cómo quedará :',
       saveMessage: 'Acaba de copiar su firma en el portapapeles.',
       signatureInstructions: `Para agregar o modificar su firma en Gmail, siga estos pasos simples:
 
-1. Haga clic en el icono de engranaje en la esquina superior derecha de su bandeja de entrada de Gmail.
+1. Haga clic en el icono de engranaje en la esquina superior derecha de su bandeja de entrada de Gmail. {gmail}
 2. Seleccione "Ver todas las configuraciones" y vaya a la pestaña "General".
 3. Desplácese hacia abajo hasta la sección "Firma" donde puede crear o editar su firma personalizada. Pegue el texto que tiene en el portapapeles en este lugar.
 4. Guarde los cambios en la parte inferior de la página para aplicar su nueva firma.`,
@@ -101,7 +113,83 @@ const translations = {
   ¡Hasta pronto!
 
   Atentamente,`,
+      linkFacebook: 'https://www.facebook.com/sweeek.es',
+      linkSweeek: 'https://www.sweeek.es',
     },
+    de: {
+      signature: "Ihre individuelle Unterschrift {brand}",
+      firstName: "Vorname",
+      lastName: "Nachname",
+      jobTitle: "Position",
+      email: "E-Mail",
+      phone: "Telefon",
+      uploadImage: "Benutzerdefiniertes Bild hochladen",
+      copyButton: "Klicken Sie zum Kopieren",
+      currentEmail: 'sduvivier@sweeek.de',
+      currentJob: "Fullstack-Entwickler",
+      gmail: "Zu Gmail gehen",
+      saveMessage: "Ihre Unterschrift wurde in die Zwischenablage kopiert!",
+      examplePreviewEmail: "Hier ist eine Beispiel-E-Mail:",
+      signatureInstructions: `So fügen Sie Ihre Signatur in Gmail hinzu oder ändern sie:
+      1. Klicken Sie auf das Zahnrad-Symbol oben rechts in Ihrem Gmail-Posteingang. {gmail}
+      2. Wählen Sie "Alle Einstellungen anzeigen" und gehen Sie dann zum Tab "Allgemein".
+      3. Scrollen Sie nach unten zum Abschnitt "Signatur", wo Sie Ihre individuelle Signatur erstellen oder bearbeiten können. Fügen Sie den Text ein, den Sie in der Zwischenablage haben.
+      4. Speichern Sie die Änderungen unten auf der Seite, um Ihre neue Signatur anzuwenden.`,
+      exampleMessage: `Hallo {firstName},
+      
+      Ich hoffe, du hast bisher einen schönen Tag! Ich hatte ein paar Ideen, wie wir unsere Arbeitsweise bei Projekten verbessern könnten, und wollte sie gerne mit dir teilen.
+      
+      Wöchentliche Besprechungen: Ich denke, es wäre toll, jede Woche schnelle Besprechungen abzuhalten, um über Fortschritte und Herausforderungen zu sprechen. Was hältst du davon?
+      Kontinuierliches Feedback: Mir gefällt die Idee, regelmäßiges Feedback zu fördern. Es kann wirklich helfen, Probleme zu lösen, bevor sie groß werden.
+      Schulung und Entwicklung: Wir könnten auch informelle Schulungssitzungen organisieren. Ich bin sicher, jeder von uns hat Tipps und Tricks, die er teilen könnte.
+      
+      Wann passt es dir diese Woche, dich zu treffen und dies persönlich zu besprechen? Ich bin Mittwochnachmittag und Donnerstagmorgen verfügbar. Natürlich bin ich flexibel, falls diese Tage nicht passen!
+      
+      Vielen Dank im Voraus für deine Zeit. Ich freue mich darauf, deine Gedanken zu diesen Ideen zu hören.
+      
+      Bis bald!
+      
+      Herzliche Grüße,`,
+      linkFacebook: "https://www.facebook.com/sweeek.de",
+      linkSweeek: "https://www.sweeek.de"
+    },
+    nl: {
+      signature: "Uw persoonlijke handtekening {brand}",
+      firstName: "Voornaam",
+      lastName: "Achternaam",
+      jobTitle: "Functie",
+      email: "E-mail",
+      phone: "Telefoon",
+      uploadImage: "Upload een aangepaste afbeelding",
+      copyButton: "Klik om te kopiëren",
+      currentJob: "Fullstack-ontwikkelaar",
+      currentEmail: 'sduvivier@sweeek.nl',
+      gmail: "Ga naar Gmail",
+      saveMessage: "Uw handtekening is zojuist gekopieerd naar uw klembord!",
+      examplePreviewEmail: "Hier is een voorbeeld van een e-mail:",
+      signatureInstructions: `Om uw handtekening toe te voegen of te wijzigen in Gmail, volgt u deze eenvoudige stappen:
+      1. Klik op het tandwielpictogram rechtsboven in uw Gmail-inbox. {gmail}
+      2. Selecteer 'Alle instellingen bekijken' en ga vervolgens naar het tabblad 'Algemeen'.
+      3. Scroll omlaag naar het gedeelte 'Handtekening' waar u uw aangepaste handtekening kunt maken of bewerken. Plak de tekst die u in uw klembord heeft op deze plek.
+      4. Sla de wijzigingen onderaan de pagina op om uw nieuwe handtekening toe te passen.`,
+      exampleMessage: `Hallo {firstName},
+      
+      Ik hoop dat je tot nu toe een goede dag hebt gehad! Ik had een paar ideeën om onze manier van werken aan projecten te verbeteren en wilde die graag met je delen.
+      
+      Wekelijkse vergaderingen: Ik denk dat het geweldig zou zijn om elke week snelle vergaderingen te houden om de voortgang en uitdagingen te bespreken. Wat vind je daarvan?
+      Voortdurende feedback: Ik vind het idee van regelmatige feedback aanmoedigen leuk. Het kan echt helpen om problemen op te lossen voordat ze groot worden.
+      Training en Ontwikkeling: We zouden ook informele trainingsessies kunnen organiseren. Ik weet zeker dat elk van ons handige tips en trucs heeft om te delen.
+      
+      Wanneer ben je beschikbaar om deze week te ontmoeten en dit persoonlijk te bespreken? Ik ben woensdagmiddag en donderdagochtend beschikbaar. Natuurlijk ben ik flexibel als deze dagen niet uitkomen!
+      
+      Alvast bedankt voor je tijd. Ik kijk ernaar uit om je gedachten over deze ideeën te horen.
+      
+      Tot snel!
+      
+      Met vriendelijke groet,`,
+      linkFacebook: "https://www.facebook.com/sweeek.nl",
+      linkSweeek: "https://www.sweeek.nl"
+    }
   };
 
   export { translations };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,16 @@
 <script>
   import { onMount } from "svelte";
-  import { Fr, Es, Gb } from 'svelte-flag-icons';
+  import { Fr, Es, Gb, Nl, De } from 'svelte-flag-icons';
+  import Fa from 'svelte-fa/src/fa.svelte'
+  import { faCheck, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+  import { faFacebook, faFacebookSquare, faGoogle, faInstagram, faPinterestSquare, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 
   import {translations} from '../lib/translations'
   let firstName = 'Sacha';
   let lastName = 'Duvivier';
   let currentLanguage = 'fr';
   let jobTitle = translations[currentLanguage].currentJob;
-  let email = 'sduvivier@sweeek.fr';
-  // import Fa from 'svelte-fa/src/fa.svelte'
-  // import { faInstagram, faSquareXTwitter,faLinkedin } from '@fortawesome/free-brands-svg-icons'
+  let email = translations[currentLanguage].currentEmail;
 
   let phone = '+33123456789';
   let logoUrl = '';
@@ -19,8 +20,8 @@
 function changeLanguage(language) {
   currentLanguage = language;
   jobTitle = translations[currentLanguage].currentJob;
+  email = translations[currentLanguage].currentEmail;
 }
-
 
 
   let logoFile = null;
@@ -35,7 +36,9 @@ function changeLanguage(language) {
       reader.readAsDataURL(file);
     }
   };
-const clickToSave = () => {
+const clickToSave = (event) => {
+  event.preventDefault();
+
 function listener(e) {
   e.clipboardData.setData("text/html", document.querySelector('.signature').innerHTML);
   e.clipboardData.setData("text/plain", document.querySelector('.signature').innerHTML);
@@ -69,12 +72,15 @@ banner = translations[currentLanguage].saveMessage;
 
 <div style="font-family: Quicksand;">
 {#if banner}
-<div class="banner bg-green-300 text-black p-4 font-bold text-5xl max-md:text-3xl absolute w-full top-0 transition-all animate">{banner}</div>
+<div class="flex gap-3 banner bg-green-300 text-black p-4 font-bold text-4xl max-md:text-3xl fixed w-full top-0 transition-all animate"><Fa icon={faCheck} />  {banner}</div>
 {/if}
 <div class="mt-10 flex gap-3 mx-6">
   <Fr on:click={() => {changeLanguage('fr')}} />
   <Gb on:click={() => {changeLanguage('en')}} />
   <Es on:click={() => {changeLanguage('es')}}  />
+  <De on:click={() => {changeLanguage('de')}}  />
+  <Nl on:click={() => {changeLanguage('nl')}}  />
+
 </div>
 <h1 class="mt-8 text-6xl max-md:text-5xl p-4 font-bold" style="line-height:80px;">{@html translations[currentLanguage].signature.replace('{brand}', '<div class="bg-black color-white w-fit inline px-2 text-white">sweeek.</div>')}</h1>
 <div class="p-4 border border-black m-4 flex gap-5 flex-wrap">
@@ -91,7 +97,7 @@ banner = translations[currentLanguage].saveMessage;
   min-width: 100%;
   font-family: 'Trebuchet MS', sans-serif;
   ">
-      <tr style="display:block;">
+      <tr style="display:block; padding-bottom: 1rem;">
       <td style="padding: 0 1rem;">
   {#if logoUrl}
     <img width="250" style="padding-right: 2rem;" src={logoUrl} alt="Logo" class="logo">
@@ -104,10 +110,28 @@ banner = translations[currentLanguage].saveMessage;
       <div class="label" style="font-weight: 700;">{firstName} {lastName}</div>
 
       <div class="label">{jobTitle}</div>
-      <div class="label">{email}</div>
-      <div class="label">{phone}</div>
+      <div class="label" style="text-decoration: none; color: white;">{email}</div>
+      <div class="label" style="text-decoration: none; color: white;">{phone}</div>
   </td>
 
+</tr>
+  <tr>
+  <td style="background:none; border:solid 1px white; border-width:1px 0 0 0; height:1px; width:100%; margin:0px 0px 0px 0px;">
+    <a style="text-decoration:none;color:white;" href={translations[currentLanguage].linkSweeek}>{translations[currentLanguage].linkSweeek.substring(9)}</a> -
+    <a href="https://instagram.com/sweeekofficial/">
+    <img width="15" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAAAXNSR0IArs4c6QAABTlJREFUaEPdmlnMXVMUx39/w4P5qYbSmodQoY2YUqTEi0pIDC3a6iAaWuMLFSEkhieEmhraKjWVhAQvao7Wg6ihJWmrQmoIL8Z6MCz53+zz5eTk3nvOPd+5X+5nPfTk69l77fVfe+21/nudK5JExK7ANcAFwGHATtm7AXn+CWwEngful/S77ZL/iQgb/Bpw8IAYW2bGZuAsSZuUPP/xKDI+A7cJmGgANwF3lEEe0PeLDMDeP2ZADSwza50BbBvAA1tmePZ+mwFE1dGDOG44AAz8O+Br4AfgnxzA34C/C4B3AHbL/d/2wN7A/sDYLCP26qQ6AD5zHgZekWTDhy0RYSBnpzo0oReFvQD4C7gWeETSv70sUnVsRGwHXAHcC+xYZV5VADbehWN1XmlEjAcOAsYAuwMOi0xszB7pj1+APGiH26/AT8AWSd8U9J4JvFoFRFUACyQ9lC0SETOBG4CjqnipwpgNwN2SnsqtsQBYXDa3CgDH/LFZ2ETEA8DCMsU13y+WdJXnpnD6BOh6JqoAuEzS40mpPb+ipnHtpr2bvHwIcDOwMzBD0sq03jzgsW7rlQFwqhybZZuIWN9g2PwMjJfklGuPZ5Rmg6SW1yNiH+Dbbim2DMBWSeOSMh9Y5/wq8gewJtUJj98XODl5OJu/UdLhuZifBjyb/h4naWta10/PbytlANZK8sL2xhTgzRLrtwC3Ai9KMn8fkojw/eJ84DbgQKsEpklaFRHOYM46k9OEKZLeTuvaESfVBfCypHOToouAp7sAsPfmFg0vjo8Ix/ky4MIEwtx+r5SGs+EXS3omrfsScE5dAPakveYduBRY3kGRjfeiLV4VEa4H3rmM5ZrxrsllMl+kPMcg2slsSU8kXS8A59UF8Jyk6UnRXKCVjQrisJmQeT4ijk8ePrIwzglgjqQPkz7vhPP/AW10zpO0NI0zUJ+PtlJ2BlZKmpEUXQ482kbLzKwARcQJwFtd6Lmp+2k5ELOAlqcLMl/SkrSui9sldQGskOTQcVhcCTxYUORsM8beT2HzKVD0fHHtocKYzoPphHcjL0OVPyIM0EBr7cBSSS4mBuAKaRaal9WSzFv8/hTAhamKTJb0fppnfnVGYdLVklzxrddh6/CtBWCJpPlJ0XXAPQUtyyS1lHcA2GndhZJauxkRzkizCwOvl2RG6vcOW4dvXwAslzQnLVSJfCUrykKkMQC9hJCL0HtV4scFKxdCbwCn9yuEejnE5v+l7BHwQZ/omjASh7hKGp0l6ckURscB77TJKpmDnbVOlfRRGu/Y9xkoSmNptEoh+yoVMud4HzqDsFFFHm/Pu5Blxu8CfG5G2gZAY4WsKpVww3V6jko4nE4EJiXjbPQHBSrhOS2a0kYaoxK9kDkbZA+3dqKTRIQ9b07VyXhPbYzM9UqnfV+4BVjVhk672rp1f3uHsMljboxO173QeBfWAq1LCbBfYqdVvzk0dqHp55WyU5Stl3R0SgjDvlJaT/5Sb2baSpl9lEYv9bZz1LdVDGKIfKWtNT9f1GCHwhebu7J2SlrDvacWI+0mZReabK5bi1MlvZ5XFhE+nP6utmfN1uKPwJdZByLTHRGNtxat2yBMqR/uc3PXFyfT9kabu3nH+257n79qSvq+bIurvE8NrKn9bq8XbXGKNQAXLz/rfOBwmvQHDj9bn3x7lapnoFe9Izb+f/GRb9R/ZnU+v3PE9rzZhW7MfmqwDnCPfjSJf/gxKfuxx6GpO+znaBAb78K6eSh1pYuGm1duuB4xgF/v3a7/Iv3cxp+ifL/mP8KQ4TIEcTSgAAAAAElFTkSuQmCC" alt="Instagram" style="display: inline-block; width:15px;height:15px;" />  
+    </a>
+    {#if translations[currentLanguage].linkPinterest}
+     <a href={translations[currentLanguage].linkPinterest}>
+      <img width="15" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAAAXNSR0IArs4c6QAABcJJREFUaEPVmnmoVVUUxn9fo83ZgBplQYNGE4qRQRPNKSUNlhIVpGVEZWlBRlk0EdFgmtlARWVmg/VHk9EgRtJAZXOURkEzFBWVza34HufKuefu++65517ltf7wyT1nr72+vdf+1rCPyCQiNgQmA2OBnYD1as/6yN/fgI+Bh4CZkn6xXfI/EWGDnwK27yPGtjJjOTBK0jJlK//W/8j4GrhlwDADuAi4qhXkPvp8mgF49ffoowa2MmupAazogwe2leG15ysMIMq+3RffWx0AfjTRARsBa3V7EboJ4F9gCfAc8HLG2V9I+icXa7YEBgAjgcOAg4FNOwHVDQDfAbcAt0n6qh1jImJt4CTATFgpBnUCwCs+C7hMkt2kskSEXes04Pp2CaUqgO+BcZLsLl2TiDCdPwzsWFZpFQB2k4MlfdhskojYDtgP2AoYCPwNfJ2di0W1PCY1PiJ8Jl4EdisDol0APwH7Snq3qDxzgwnAmcDuvUz+J/AEcLmkt5uA2DojAv/tVdoFMF7S/ITxI4B7gZ1bTZh7bmq1z0+T5B2qk4iwzleANXvT2Q6AxyUdlZhoDHA/sEEbxudfdRY8pgmIO4CJ3QBgxtm16PcRsRewGFi3ovG1YbMknZNYHMcMZ50OgkkpuwMLJR2R1xARGwPvAyk/dbHxGTAI2LwEOLvTSEmvJUDcCkzqFMAkSbcXAFzig1hQbF+eDtwkaUVErJHx++xWvgw8I+nwBIADgEWdAhgs6fOakoiwy5gW+xcUny7Jfls8kPcAJ7fYCaccAyQ5xqyUiFgHcKBMlrhlXGiZJJeceaXOYZ4tGLRYklerQSJiFPBkCVc6QZJr3uICvAEMT40vA8CB58ACgKtNfwWFSYr1OxExDHizBABT6jUJAI8CR1cFMF/S+AIAc76TsLwMkvRNkx0YnQWvVhh8ds5NAEjN1/NamR2YI8nRNe9Cdge7RU3+MpVKShZHEXEBcG0r64EbJU1JAFgAHFN1B+6TVHcAI2IekN8Vpwf9egHwNNDAMAmDpku6IgHAsca5VYOU2YElkvYp7IAnubigbTNJPyQmN2OZWcpE6mYs9kGzNKUMAAclG2c36ZGI8KF+vmDsIan0OiIONceXcB+/MkKSGSfvrqZPL0wy2pcBYGVOn1caHBFOsD4Bts3NNVvSWYkdmJG1LFthcHekvyS7Yx5AVwLZXEl1rBMRZwBzcnPNkHReAoD7mWUKlHmSTkyMv9RVXzP0ZXfAqzJU0qc5N3Ka8Kq3PfttrKRHCqtnww2gjDS4YES4d+t8q2maXhaADXhMUh2VRYSjsaOyZaCkbwsAzFRmrFbygqSDEqtv5jKDNZV2AFjJZEkz/Z+so+DDZXZpSDeyd44F6nYlYcnPWSZqpqmTwgIlQbQLwIHqfAccwLWA+z+WuyWdmjDA9a3T6k2aLOHvThEkLUyMHQc80Grr2gVQ02fDfS72z36YIOmu1GQRcSTgMnT9wvN3gFMkublcXHkXMu8BW6wqAEW9QyQ1PawRsU0Wud2t+DLr4Dl7bUg9skLJO7J3K+P9vOoO5HX74DqRqzPGNYOkP8oYUXunXeO7BWCBpOPyhma3Pg82K9abuJrzfTcHhrYDuhs7MEWSD/VKyRUwjhM+H+bypETEEGAqYBLotYWSUtANAHtKer0A4LrMKP9s13op43Mz0q/OrYBdAOdULnZ6LhurSKcAnOg5f6lrTK3Oa6tOATRE0IjwHYArM6caq1w6BdDQkIqI4wEf4NUinV7ypQ7wXKAhq1xFaHou+Tq5Zq1reGX06fZ701Zgl4H0XLO6PeI2SRWZKOnO2sCIONvfMVRRVHHMhQbgjzyWAjtUUDJV0g09XBnRD/gIGFxBT5UhTl2G1z72cOHhVkmZyik/2ZWS3CM1AAcj8//qEBs/WtLylQEkIpzX2wXMIg7nZT636amDs2shf0FSphNdFaA/t/G1lluPN0tyQOQ/QRaVMhTRrDIAAAAASUVORK5CYII=" alt="Pinterest" style="display: inline-block; width:15px;height:15px;" />  
+    </a>
+    {/if}
+    {#if translations[currentLanguage].linkTwitter}
+    <a href={translations[currentLanguage].linkTwitter}><img width="15" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAAAXNSR0IArs4c6QAACHRJREFUaEPVWgdsU1kWvTYx9ho7oShLSaEFhEkwEJoQTWYAUQQZqgChoQlE76ETZCAjQEvvIAi9OBF9IkQQXYBoBhNAJCxLIAXZJAqJwSS249G5s99iSfsubIYnRbZ57793z7333PI+EvpuGAyGf2i12nCFQqGUSCTS7+er6rfT6XTk5eWZCwsLLTqdziHIIflWoNTU1NbBwcG/qVSqX6RSaQ0i+tsAICJHcXFxxqdPn84YjcajMTExhZCdARgMhuqtW7f+JTw8/KBCofhnVWlZzLkul4vy8/MNWVlZC5KSkrIYwMuXL3uEhITEqdXqnmI2qeo1Dofjs9ls3mCxWPZKUlJSgqKjo3cFBgbGBAQEKKtaOLHn22y2zIKCgliJyWRq0qRJk1NKpbKdRCL5H06I3awq1tnt9i+5ublxkrS0NE1YWJhBLpdH/UTyk91uLzKbzWsk6enpkaGhoQaFQtGyKjTp7Zl2u704Nzf39woBOJ1OKiws5L+KRvXq1alOnToUEBDgrTz8XElJCX348IFwrlKppNq1a1N5XiEKQHFxMT148IDmzZtHr169Kle4wMBAWrFiBU2aNMlrAF++fKEDBw7QypUrqWbNmvy9e/fuvgGANABx6NAhmjp1KmsGwqpUKreg0JrFYqGIiAi6fPkyhYeHewXiyJEjtGzZMrJarbR06VKaM2dOhRYVZQFBEoCYOXMmA+nYsSNt27aNBYV5AWD27Nl08uRJ6tu3L23fvt09JwbJ58+faffu3aTX6wlJasOGDTR27FiSy+UVPu4RAOx048YNmjJlCmt7zZo17C7VqlXjQy5evMgaKygoYAEGDRpEQUFBlcpvt9vpypUrbN3s7Gzq3bs3HT58mPlU2fAYQFFREe3Zs4f5EBISwhrv3Lkzn/P161fWIuaaN2/On5MnT65MBrp16xZbz2g0sr9v3ryZ2rZtW+lzWOAxgP8+xK6UkJBA9evX588ePXqQVColkBBCJyYmUq1atdgq7dq1K5OEcLuHDx/SyJEj6f3799SvXz/aunUrNWrUSJTwXgPAgyaTid3n/v371L9/f9Z8WFgYH/z48WOaOHEiPXv2jAYOHMhcCQ0NLSXU8+fPaf78+ew+4M3OnTs9Jr9XFhAkefr0KQ0dOpQyMjJo2LBhtH//fo7bIOHt27dp3bp1HJHGjx/PmhUIifl3797RkCFD6MmTJ2wBrC0LZGWm8AkAXODcuXM0ffp0lLYMYMSIEUxqh8NB9+7dYyLjO+YgMOYyMzNpyZIldPz4cWrfvj2dOHGCGjduXG6srwiETwCEjRFWY2NjOS8gfMKlhLFjxw5OSnXr1mUXQcTB54ULF0in03G0atWqVWWKLnfeLwDgDiB1cnIyu9LBgwfd7oK5adOmUUpKCrsKQizcCmUHsiw44kvp4RcA8GmUGjExMVwvQTC4CwSDm12/fp1dC9kVv8GTuXPnshsBiC/DLwAEAfbu3ctlAFwJ37t160YKhYKnd+3axaUBsjm0jt8Is74OvwIAkePj42nTpk3UtWtXWrhwoZsPZrOZli9fzmRu1qwZJ0PkDl+HXwFAGJQYgwcP5giEbHzp0iV3bP/48SMDQp4AcZHkkM19GX4HAGFAWGRjgcCrVq1yu8uZM2c45oMzixcvZh58W9V6CuaHAECkmTFjBp0+fZpkMhln4jFjxrBs4MDVq1dpwoQJHFKR4EaNGuWp3O71fgeAKHP+/Hku5JChMSIjI/nfhBoHkWrt2rW0fv160mq1HH6RJ7wZfgeAGmn48OH0+vVr0mg0VKNGDS7YYJHVq1dzI4QBUiMaPXr0iEMqCC6m9P4epF8BIN4vWrSIUlNTafTo0dw3oEIFH9LS0jhbw+eF/uHmzZucAN++fcvZGr0E1nsy/AYATTiy8N27dzmhoURo2LAhJy7UOqhcoX1k4TZt2rCM4MDRo0d5DmU5EiCaGU+GXwBA47NmzeLGBAKgSENDLgxkarjIxo0bKTo6mhYsWOAuIVDooaRG4gNANEhCbyEGiM8AsrKymLBnz56lLl26cIjs0KFDqbPT09PZXa5du8YV6L59+6hly7+uoOBecDfM9ezZk1AAtmjRQoz83nVkws5oIRHjUV2i1RRqoLIacYRPhFX0Bgit4AL+hNCKrg4AUSdhT1hUzPDaAmi+UTagE6tXrx7FxcUxWSu6lgQfYC1cnQAw2s4+ffq4+wdYBeUHCjy4Ya9evdyELw+MVwBAPlx/bNmyhZsVXGZBY2Iy6ps3b9jfkcA6derE1kO4xbDZbByJ0F+gEMQ6NDoVDY8BCFcm0H5wcDBnWZTOnoQ/1Esg7rFjx9gCSUlJnC8wMIcbilOnTnE3hzVwK79YANEEVx4gKmp9xO5x48axT3sysA+IO2DAAEIQwP0SkpqQH3C9gtAKi8AKyCnlNT2iLYDNoCn4ObQFIOiwfOmmEDKR3HJycriD+5Y/CBC4wsRNB9wNfCjLRUUBgJ+/ePGCD0Pqh2nh/2q12hPFl1qL8hoNENylPPLDNVGegxtRUVGl9hAFACEQpTHucbAhMqlwB+QTAiIu+KCUigY4gIKwrDNFAfBVyB/5/LcAWuINjVwuj/zJXjEVWyyWeInRaGwUERFxQqlUdpJKpT/TSz5rXl7econBYFDpdLrNQUFBI2QymW/s/JE+893eNpvt3/n5+bGscZPJ1L1p06a/K5XKLv9HGbw+yul02rOzs/+VmZm5gwEkJyfLNRrNkAYNGmyUyWR1/87vi0tKSpxWq/ViRkbGPK1W+x+3zwNEVFTUr2q1eoZKpQIfPEuzXutT/INFRUUWq9X6h9lsjk9MTHyj1+tLSpE2ISFBodFoIlUqVbDL5fLtval42Spd6XK5vubk5Ly6c+dOjl6vd/93mz8BHs1dk6AkmIIAAAAASUVORK5CYII=" alt="X" style="display: inline-block; width:15px;height:15px;" /></a>
+    {/if}
+    <a href={translations[currentLanguage].linkFacebook}>
+      <img width="15" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAAAXNSR0IArs4c6QAAAmJJREFUaEPtmrtrFUEUh78fFj4RRQ2S0vejEMXCBzaCjZaihZ0PEESxNTY26h+gliJ2QQs7rYNE0EJiZcTE0iqaJiZCjB45l12VcO/O3utO7izsgb1bzNzd33fOnjOzOyMyM7M1wHXgDLADWJm3JXL+DnwEngL3JH1zXfIfM3PBL4CtiYgNyZgETkqaUOb5dzUSn8NNAPsd4CZwJ4ScaPuQA7j39yUqMCRrzAHm+pywM8DXTOk6wI+yNucAVrZ3Rf0WgCfAM2BE0vTi65rZemAQOAg8LrrvUgOMAJckfSrjDDNziM+pADwCLkvyCJSylABeAcclzZdS/ndwTSICP4HdPuh0Iz4bYJMAGJZ0rlvxKQGcluQVp5SZ2SpgedZ5M/C+n0n8C9jUrlT+K8rM9gC3gROATypLW+wyOiVpoEiNmW0B3nY5gP25ZGyAcUnu3Y5mZveBq6VdvqhjbIA3kg4FAF4Cx1IFeC3pcADgA7CzAejVA4H/NREIOTZ2EtciAj+ATok6K8mTtKiM7gVWdOhwAbgSeySel5QP/aGId9VuZj4FP19nAJ+GH6kzwBdgQy0BzGwjMBV65qqoQlFywMyOAqN1BrgIPFwSAMBfPNrZgiT/7lNURtcCy9p0uJV9bC5kqOIRKrpBLQayBqCZTocy/T/amxwIOa+pQs0bmVnzUh9zhaapQmWqUMxFvtgRaC3yxVxmjQ3QWmYdAu6GQtVje2yAG/lWgzFgW48i+zUb9Y0fB/LNHtuB54Cfq7RYEXDxpyRNtgDczGw1cA04C+yqaPW+SgDfbjOebbd5IGnWdf8GkZyyIxcjNxYAAAAASUVORK5CYII=" alt="Facebook" style="display: inline-block; width:15px;height:15px;" /></a>
+  </td>
 </tr>
 </table>
 </div></div>
@@ -138,7 +162,7 @@ banner = translations[currentLanguage].saveMessage;
   <button class="bg-black text-white p-4 mt-4 font-bold" on:click={clickToSave}>{translations[currentLanguage].copyButton}</button>
 
   <p class="mt-6">
-    {@html translations[currentLanguage].signatureInstructions.replace(/\n/g, "<br/>")}
+    {@html translations[currentLanguage].signatureInstructions.replace(/\n/g, "<br/>").replace('{gmail}', `<a href="https://mail.google.com/mail/u/0/#settings/general" target="_blank" class=" border items-center w-fit border-black whitespace-nowrap hover:bg-black hover:text-white transition-all px-1 mt-1">${translations[currentLanguage].gmail}</a>`)}
   </p>
 </form>
 </div>
