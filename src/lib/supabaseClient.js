636208a35536111b24c6813862f586e4a2398fef
Sacha Duvivier <sduvivier@walibuy.com>
2024-02-11 21:12:39 +0100

    fix

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-const { SUPABASE_URL, SUPABASE_ANON_KEY } = process.env
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
